459601d0d70aebf81d737fb42b3c7c583f6592ef
Jered Ye <87924680+JeredYe@users.noreply.github.com>
2023-02-13 11:22:08 +0800

    Add files via upload

---
+++ AutomaticVideoSaver/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace AutomaticVideoSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Preserve = new System.Windows.Forms.RadioButton();
+            this.Bilibili = new System.Windows.Forms.RadioButton();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.delete = new System.Windows.Forms.CheckBox();
+            this.tOpen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tSave = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(29, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(350, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(28, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(351, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "一键拷贝所下载的视频";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.Description = "请选择视频所在目录";
+            this.openFile.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Description = "请选择存放目录";
+            this.saveFile.HelpRequest += new System.EventHandler(this.saveFile_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "检测到 0 个视频";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "注意：重复文件名会默认覆盖";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Preserve
+            // 
+            this.Preserve.AutoSize = true;
+            this.Preserve.Checked = true;
+            this.Preserve.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Preserve.Location = new System.Drawing.Point(28, 282);
+            this.Preserve.Name = "Preserve";
+            this.Preserve.Size = new System.Drawing.Size(117, 24);
+            this.Preserve.TabIndex = 4;
+            this.Preserve.TabStop = true;
+            this.Preserve.Text = "保留原文件名";
+            this.Preserve.UseVisualStyleBackColor = true;
+            // 
+            // Bilibili
+            // 
+            this.Bilibili.AutoSize = true;
+            this.Bilibili.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bilibili.Location = new System.Drawing.Point(218, 282);
+            this.Bilibili.Name = "Bilibili";
+            this.Bilibili.Size = new System.Drawing.Size(161, 24);
+            this.Bilibili.TabIndex = 5;
+            this.Bilibili.Text = "根据BilibiliWin10端";
+            this.Bilibili.UseVisualStyleBackColor = true;
+            this.Bilibili.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 355);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(388, 26);
+            this.progress.TabIndex = 6;
+            this.progress.Click += new System.EventHandler(this.progress_Click);
+            // 
+            // delete
+            // 
+            this.delete.AutoSize = true;
+            this.delete.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.delete.Location = new System.Drawing.Point(263, 235);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(116, 18);
+            this.delete.TabIndex = 7;
+            this.delete.Text = "复制后删除文件";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // tOpen
+            // 
+            this.tOpen.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tOpen.Location = new System.Drawing.Point(114, 102);
+            this.tOpen.Name = "tOpen";
+            this.tOpen.Size = new System.Drawing.Size(205, 25);
+            this.tOpen.TabIndex = 8;
+            this.tOpen.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "视频所在目录:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "视频保存目录:";
+            // 
+            // tSave
+            // 
+            this.tSave.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tSave.Location = new System.Drawing.Point(114, 137);
+            this.tSave.Name = "tSave";
+            this.tSave.Size = new System.Drawing.Size(205, 25);
+            this.tSave.TabIndex = 10;
+            this.tSave.TextChanged += new System.EventHandler(this.tSave_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(325, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 56);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "刷新";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 393);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tOpen);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.Bilibili);
+            this.Controls.Add(this.Preserve);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "视频自动收集器ByTangent Beta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog openFile;
+        private System.Windows.Forms.FolderBrowserDialog saveFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton Preserve;
+        private System.Windows.Forms.RadioButton Bilibili;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.CheckBox delete;
+        private System.Windows.Forms.TextBox tOpen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tSave;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AutomaticVideoSaver/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace AutomaticVideoSaver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String Pathfile = "D:/AutoDownPath.dat";
+        private String OpenPath="", SavePath = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "视频自动收集器ByTangent Beta"+System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            if (Read(Pathfile)!="")
+            {
+                StreamReader sr = new StreamReader(Pathfile, Encoding.Default);
+                OpenPath = sr.ReadLine();
+                SavePath = sr.ReadLine();
+                sr.Close();
+                DetectList();
+                //               MessageBox.Show("原目录已读取：\r\n视频目录：" + OpenPath + "\r\n存储目录：" + SavePath, "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                tOpen.Text = OpenPath;
+                tSave.Text = SavePath;
+                
+            }
+        }
+
+        List<String> VideoPaths = new List<string>();
+        List<String> VideoTitles = new List<string>();
+        List<String> VideoDirectories = new List<string>();
+        public List<string> GetVideoList(string sSourcePath)
+
+        {
+
+            List<String> list = new List<string>();
+
+            //遍历文件夹
+
+            DirectoryInfo theFolder = new DirectoryInfo(sSourcePath);
+
+            FileInfo[] thefileInfo = theFolder.GetFiles("*.mp4", SearchOption.TopDirectoryOnly);
+
+            foreach (FileInfo NextFile in thefileInfo)  //遍历文件
+
+                list.Add(NextFile.FullName);
+
+
+            //遍历子文件夹
+
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+
+            foreach (DirectoryInfo NextFolder in dirInfo)
+
+            {
+
+                //list.Add(NextFolder.ToString());
+
+                FileInfo[] fileInfo = NextFolder.GetFiles("*.mp4", SearchOption.AllDirectories);
+
+                foreach (FileInfo NextFile in fileInfo)  //遍历文件
+
+                    list.Add(NextFile.FullName);
+            }
+
+            return list;
+        }
+
+        public List<string> GetVideoTitle(string sSourcePath)
+
+        {
+
+            List<String> list = new List<string>();
+
+            //遍历文件夹
+
+            DirectoryInfo theFolder = new DirectoryInfo(sSourcePath);
+
+            FileInfo[] thefileInfo = theFolder.GetFiles("*.mp4", SearchOption.TopDirectoryOnly);
+
+            foreach (FileInfo NextFile in thefileInfo)  //遍历文件
+
+                list.Add(NextFile.Name);
+
+
+            //遍历子文件夹
+
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+
+            foreach (DirectoryInfo NextFolder in dirInfo)
+
+            {
+
+                //list.Add(NextFolder.ToString());
+
+                FileInfo[] fileInfo = NextFolder.GetFiles("*.mp4", SearchOption.AllDirectories);
+
+                foreach (FileInfo NextFile in fileInfo)  //遍历文件
+
+                    list.Add(NextFile.Name);
+            }
+
+            return list;
+        }
+
+        public List<string> GetVideoDirectory(string sSourcePath)
+
+        {
+
+            List<String> list = new List<string>();
+
+            //遍历文件夹
+
+            DirectoryInfo theFolder = new DirectoryInfo(sSourcePath);
+
+            FileInfo[] thefileInfo = theFolder.GetFiles("*.mp4", SearchOption.TopDirectoryOnly);
+
+            foreach (FileInfo NextFile in thefileInfo)  //遍历文件
+
+            {
+                list.Add(NextFile.DirectoryName);
+                
+            }
+                
+
+            //遍历子文件夹
+
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+
+            foreach (DirectoryInfo NextFolder in dirInfo)
+
+            {
+
+                //list.Add(NextFolder.ToString());
+
+                FileInfo[] fileInfo = NextFolder.GetFiles("*.mp4", SearchOption.AllDirectories);
+
+                foreach (FileInfo NextFile in fileInfo)  //遍历文件
+
+                    list.Add(NextFile.DirectoryName);
+            }
+
+            return list;
+        }
+        public List<string> GetBilibiliTitle()
+        {
+            StreamReader sr = null;
+            String TempPath = "";
+            for (int i=0;i<VideoPaths.Count;i++)
+            {
+                TempPath = VideoDirectories[i].Substring(0,
+                    VideoDirectories[i].Length
+                    -"1\\".Length
+                    );
+ //               MessageBox.Show(TempPath);
+                sr = new StreamReader(TempPath+"\\desktop.ini", Encoding.Default);
+                sr.ReadLine();
+                VideoTitles[i]=sr.ReadLine();
+                VideoTitles[i] = VideoTitles[i].Replace("InfoTip=", "");
+                VideoTitles[i] += ".mp4";
+                sr.Close();
+
+            }
+
+            return VideoTitles;
+        }
+
+        void DetectList()
+        {
+            VideoPaths = GetVideoList(OpenPath);
+            VideoDirectories = GetVideoDirectory(OpenPath);
+            if(Preserve.Checked)
+            {
+               VideoTitles = GetVideoTitle(OpenPath);
+            }
+            else if(Bilibili.Checked)
+            {
+                VideoTitles = GetVideoTitle(OpenPath);
+                VideoTitles = GetBilibiliTitle();
+            }
+            label1.Text = "检测到 "+VideoPaths.Count+" 个视频";
+            if (VideoPaths.Count!=0)
+            {
+                progress.Maximum = VideoPaths.Count;
+            }
+        }
+
+        public String Read(string path)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            line= sr.ReadToEnd();
+            sr.Close();
+            return line;
+            
+        }
+
+        private void WritePath()
+        {
+            FileStream fs = new FileStream(Pathfile, FileMode.OpenOrCreate);
+            byte[] byteArray = System.Text.Encoding.Default.GetBytes(OpenPath+"\r\n");
+            fs.Write(byteArray, 0, byteArray.Length);
+            byteArray = System.Text.Encoding.Default.GetBytes(SavePath);
+            fs.Write(byteArray, 0, byteArray.Length);
+            fs.Flush();
+            fs.Close();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+            if (openFile.SelectedPath != "")
+            {
+                OpenPath = openFile.SelectedPath;
+                saveFile.ShowDialog();
+                if(saveFile.SelectedPath!="")
+                {
+                    SavePath = saveFile.SelectedPath;
+                }
+                WritePath();
+                DetectList();
+                tOpen.Text = OpenPath;
+                tSave.Text = SavePath;
+                MessageBox.Show("目录已设置！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                DetectList();
+            }
+            
+        }
+
+        /*private static bool CopyDirectory(string SourcePath, string DestinationPath, bool overwriteexisting)
+        {
+            bool ret = false;
+            try
+            {
+                SourcePath = SourcePath.EndsWith(@"\") ? SourcePath : SourcePath + @"\";
+                DestinationPath = DestinationPath.EndsWith(@"\") ? DestinationPath : DestinationPath + @"\";
+
+                if (Directory.Exists(SourcePath))
+                {
+                    if (Directory.Exists(DestinationPath) == false)
+                        Directory.CreateDirectory(DestinationPath);
+
+                    foreach (string fls in Directory.GetFiles(SourcePath))
+                    {
+                        FileInfo flinfo = new FileInfo(fls);
+                        flinfo.CopyTo(DestinationPath + flinfo.Name, overwriteexisting);
+                    }
+                    foreach (string drs in Directory.GetDirectories(SourcePath))
+                    {
+                        DirectoryInfo drinfo = new DirectoryInfo(drs);
+                        if (CopyDirectory(drs, DestinationPath + drinfo.Name, overwriteexisting) == false)
+                            ret = false;
+                    }
+                }
+                ret = true;
+            }
+            catch (Exception ex)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+        */
+        private void CopyFile()
+        {
+            for (int i=0;i<VideoPaths.Count;i++)
+            {
+ //               MessageBox.Show(SavePath + "\\" + VideoTitles[i]);
+                File.Copy(VideoPaths[i], SavePath+"\\"+VideoTitles[i],true);
+                progress.Value = i + 1;
+                if(delete.Checked)File.Delete(VideoPaths[i]);
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (OpenPath == "")
+            {
+                MessageBox.Show("请先选择目录！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                button2.Enabled = false;
+                CopyFile();
+                DetectList();
+                button2.Enabled = true;
+                MessageBox.Show("完毕！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                progress.Value = 0;
+            }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            DetectList();
+        }
+
+        private void progress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            OpenPath = tOpen.Text;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tSave_TextChanged(object sender, EventArgs e)
+        {
+            SavePath = tSave.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenPath = tOpen.Text;
+            SavePath = tSave.Text;
+            WritePath();
+            DetectList();
+
+        }
+
+        private void saveFile_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
